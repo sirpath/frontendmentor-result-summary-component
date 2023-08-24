7e2dbcb54e81ff97d89a683eed418857bbd7ef24
sirpath <surapath.sk@gmail.com>
2023-08-24 16:56:18 +0700

    finish frontend mentor

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        hankengrotesk: ["HankenGrotesk", "sans-serif"]
+      }
+    },
+  
+  },
+  plugins: [],
+}
+
